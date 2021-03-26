0f19057e4b136108396688998d530ee007ce07e3
walter <belwalterv@gmail.com>
2021-03-26 18:16:40 -0300

    archivos primera clase

---
+++ ejercicio_guia1.py
@@ -0,0 +1,61 @@
+
+
+# zermaten, guiffrey
+
+# ejercicio 1
+# base = float(input('ingrese el valor de la base '))
+# altura = float(input('ingrese el valor de la altura '))
+
+# area = base * altura
+
+# print('el area del rectangulo es:', round(area, 2), 'cm')
+
+# # print('el area del rectangulo es:', base * altura, 'cm')
+
+# # ejercicio 2
+# valor_producto = float(input('ingrese el valor de la maquina dolares '))
+# valor_dolar = float(input('ingrese el valor del dolar en pesos'))
+
+# print('el valor de la maquina es pesos es', valor_producto * valor_dolar)
+
+
+# anio_actual = 2021
+# anio_nacimineto = int(input('ingrese el año de nacimiento '))
+
+# print('la edad de la persona es', anio_actual - anio_nacimineto)
+
+
+# ejercicio 6
+cantidad_m2 = int(input("ingrese la cantidad de m2 a pintar "))
+costo_litro_pintura = float(input("costo del litro de pintura "))
+
+total_pintura = cantidad_m2 * costo_litro_pintura
+mano_obra = total_pintura * 0.45
+total = total_pintura + mano_obra
+
+print('el monto total es', total)
+
+# ejercicio 7
+# from math import sqrt
+
+# lado_a = int(input('ingrese el valor del lado a '))
+# lado_b = int(input('ingrese el valor del lado b '))
+
+# # calcula la hipotenusa
+# diagonal = int(sqrt(lado_a ** 2 + lado_b ** 2)) # calcula la hipotenusa
+
+# print('la distancia en diagonal', diagonal)
+
+# ejercicio 11
+minutos = int(input('ingrese duracion de la llamada en minutos '))
+costo_minuto = int(input('ingrese costo del minuto de llamada '))
+
+total_llamada = minutos * costo_minuto
+adicional = 5 * total_llamada / 100
+
+total = total_llamada + adicional
+print(total)
+
+
+
+
---
+++ inicio.py
@@ -0,0 +1,26 @@
+
+
+#! TIPOS  DE DATOS
+#! ENTEROS INT, REALES FLOAT, CADENA DE CARACTERES STRING, BOLEANOS BOOL, COMPLEJOS COMPLEX
+#! input siempre ingresa texto string
+#! OPERADORES ARITMETICOS +, -, *, /, //, **
+
+#! FUNCIONES DE CONVERSION int() float() str() bool()
+
+numero = int(input("ingrese un numero "))
+
+print()
+
+print(numero + 2)
+print(numero - 2)
+print(numero * 2)
+print(numero / 2)
+print(numero // 2)
+print(numero ** 5)
+
+
+
+
+
+
+
