56c8e2d7cab4f3f6a6a2637bb1b665800abafe53
walter <belwalterv@gmail.com>
2021-03-26 19:00:46 -0300

    intro condicional

---
+++ clase_dos.py
@@ -0,0 +1,23 @@
+
+#! ESTRUCTURAS DE CONTROL
+
+#! CONDICIONAL SIMPLE - CONDICIONAL COMPUESTO - CONDICIONAL ANIDADO
+
+#! CONDICION --> valor|variable  operador_control  valor|variable
+#! OPERADORES DE CONTRO >, <, >=, <=, ==, !=
+
+
+numero = int(input('ingrese un numero '))
+
+if(numero < 19):
+    print('el numero es menor que 19')
+else:
+    print('el numero es mayor')
+
+
+print('fin del algoritmo')
+
+
+
+
+
