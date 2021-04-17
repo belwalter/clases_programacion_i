f5f2740cb6a1c0abfce6db1ade33be204062e79b
walter <belwalterv@gmail.com>
2021-04-16 21:20:37 -0300

    ultimos cmabios

--- clase4.py
+++ clase4.py
@@ -36,9 +36,36 @@ from mis_funciones import suma, agregar
 from funciones import positivo_negativo
 
 
-numero = int(input('ingrese un valor '))
+# numero = int(input('ingrese un valor '))
 
-if(positivo_negativo(numero)):
-    print('algo')
+# if(positivo_negativo(numero)):
+#     print('algo')
+# else:
+#     print('otra cosa')
+from funciones import contar_caracter, es_palindromo, sumatoria
+
+palabra = "neuquen"
+
+print(contar_caracter(palabra, "m"))
+
+if(es_palindromo(palabra)):
+    print('es palindromo')
 else:
-    print('otra cosa')
+    print('no es palindromo')
+
+
+print(sumatoria(6))
+
+
+#! DICCIONARIOS clave-valor
+
+json = [{'nombre':'walter', 'edad':30},{'nombre':'Tito', 'edad':30}, {'nombre':'Ana', 'edad':30}]
+
+for persona in json:
+    print(persona['nombre'], persona['edad'])
+
+dic = {}
+print(dic)
+
+dic['14'] = 'Ana'
+print(dic)
--- funciones.py
+++ funciones.py
@@ -7,4 +7,28 @@ def positivo_negativo(numero):
         return True
     else:
         return False
+
+
+
+def contar_caracter(cadena, caracter):
+    """contar cuantas veces aparece el caracter en la cadena"""
+    cantidad = 0
+    for letra in cadena:
+        if(letra == caracter):
+            cantidad += 1
+    return cantidad
     
+def es_palindromo(cadena):
+    """determina si una cadena es palindromo"""
+    cadena_aux = list(cadena)
+    cadena_aux.reverse()
+    cadena_invertida = ''
+    cadena_invertida = cadena_invertida.join(cadena_aux)
+
+    return cadena_invertida == cadena
+
+def sumatoria(numero):
+    suma = 0
+    for i in range(1, numero+1):
+        suma += 1/i
+    return suma
